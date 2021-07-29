dcd4236abbf8e2d342614ed97fd616d2a4e4f841
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-07-29 13:58:16 -0400

    Configured Open Weather API data & setup interface for TypeScript.

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './popup.css'
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./popup.css";
 
-const test = <img src="icon.png" />
+import { fetchOpenWeatherData } from "./../utils/api";
 
-const root = document.createElement('div');
+const App: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchOpenWeatherData('Toronto')
+      .then((data) => console.log('Weather data API response  :',data))
+      .catch((err) => console.log('Error while fetching weather data : ',err));
+  }, []);
 
+  return (
+    <div>
+      <img src="icon.png" />
+    </div>
+  );
+};
+
+const root = document.createElement("div");
 document.body.appendChild(root);
-
-ReactDOM.render(test, root);
+ReactDOM.render(<App />, root);
